a2c9ccb7853d3b9314a61c8700adca33d3f2921b
mterczynski <mterczynski271@gmail.com>
2021-04-03 18:54:15 +0200

    handle empty arrays

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export function generateSchemaFrom(data: string | [] | object) {
     if(typeof data === 'string') {
         return `Joi.string().required()`
     } else if(Array.isArray(data)) {
-
+        return `Joi.array().required()`
     } else if(typeof data === 'object') {
         return `Joi.object({}).required()`
     }
--- test/generateSchemaFrom.spec.ts
+++ test/generateSchemaFrom.spec.ts
@@ -7,9 +7,15 @@ describe('generateSchemaFrom', () => {
         expect(generateSchemaFrom(input)).toEqual(`Joi.string().required()`)
     })
 
-    it('should generate "Joi.object({})" from empty  object', () => {
+    it('should generate "Joi.object()" from empty  object', () => {
         const input = {};
 
         expect(generateSchemaFrom(input)).toEqual(`Joi.object({}).required()`)
+    })
+
+    it('should generate "Joi.array()" from empty array', () => {
+        const input = [];
+
+        expect(generateSchemaFrom(input)).toEqual(`Joi.array().required()`)
     })
 })
