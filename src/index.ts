05749c0377eef0dcab75dbb06ba01a09412b2ceb
mterczynski <mterczynski271@gmail.com>
2021-04-03 18:51:14 +0200

    handle strings

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export function generateSchemaFrom(data: string | [] | object) {
     if(typeof data === 'string') {
-
+        return `Joi.string().required()`
     } else if(Array.isArray(data)) {
 
     } else if(typeof data === 'object') {
--- test/generateSchemaFrom.spec.ts
+++ test/generateSchemaFrom.spec.ts
@@ -4,6 +4,6 @@ describe('generateSchemaFrom', () => {
     it('should generate "Joi.string()" from string', () => {
         const input = 'test';
 
-        expect(generateSchemaFrom(input)).toEqual(`Joi.string().requred()`)
+        expect(generateSchemaFrom(input)).toEqual(`Joi.string().required()`)
     })
 })
