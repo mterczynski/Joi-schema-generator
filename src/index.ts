efc5602f778efc19876eb19d9985e04137ece09b
mterczynski <mterczynski271@gmail.com>
2021-04-04 14:27:35 +0200

    Create a website

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
 import { SchemaGenerator } from "./SchemaGenerator";
 
+function updateOutputValue() {
+    try {
+       const output = schemaGenerator.generateSchemaFrom(JSON.parse(inputTextArea.value));
+       outputElement.innerHTML = output;
+   } catch(error) {
+       console.log('invalid input')
+   }
+}
+
+const inputTextArea = document.getElementById('input-textarea') as HTMLTextAreaElement;
+const outputElement = document.getElementById('output') as HTMLDivElement;
 const schemaGenerator = new SchemaGenerator();
+
+updateOutputValue();
+inputTextArea.focus();
+
+inputTextArea.addEventListener('input', (event) => {
+    updateOutputValue();
+});
